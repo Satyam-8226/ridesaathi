34812bc88577dd6ec1d7318b7f7342c005f7b541
Satyam Pandey <s.pandey274509@gmail.com>
2026-01-21 14:50:03 +0530

    fixed server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const startServer = async () => {
     await connectDB(); // 🔑 WAIT for DB
 
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
